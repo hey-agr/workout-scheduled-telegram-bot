2d4eda20f3f471cbd5938719565be8e1497db1dd
github_main <AGRabadanov@gmail.com>
2021-04-17 18:09:56 +0300

    added DB config

---
+++ src/main/java/com/agr/workoutscheduledtelegrambot/db/DatabaseConfig.java
@@ -0,0 +1,42 @@
+package com.agr.workoutscheduledtelegrambot.db;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.configuration.FluentConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dbUser;
+
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+
+    @Bean("flyway")
+    public Flyway flyway() {
+        FluentConfiguration fluentConfiguration = Flyway.configure().dataSource(dbUrl, dbUser, dbPassword);
+        Flyway flyway = fluentConfiguration.load();
+        flyway.migrate();
+        return flyway;
+    }
+
+    @DependsOn("flyway")
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        config.setUsername(dbUser);
+        config.setPassword(dbPassword);
+        return new HikariDataSource(config);
+    }
+}
--- src/test/java/com/agr/workoutscheduledtelegrambot/WorkoutScheduledTelegramBotApplicationTests.java
+++ src/test/java/com/agr/workoutscheduledtelegrambot/WorkoutScheduledTelegramBotApplicationTests.java
@@ -1,13 +1,19 @@
 package com.agr.workoutscheduledtelegrambot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 
 @SpringBootTest
 class WorkoutScheduledTelegramBotApplicationTests {
 
+	@Autowired
+	private TelegramBotsApi api;
+
 	@Test
 	void contextLoads() {
+
 	}
 
 }
