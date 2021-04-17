38454cee6985799316c19a699b4f04fa46652ad2
github_main <AGRabadanov@gmail.com>
2021-04-17 16:50:14 +0300

    Initial commit

---
+++ src/main/java/com/agr/workoutscheduledtelegrambot/WorkoutScheduledTelegramBotApplication.java
@@ -0,0 +1,23 @@
+package com.agr.workoutscheduledtelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class WorkoutScheduledTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutScheduledTelegramBotApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+}
---
+++ src/test/java/com/agr/workoutscheduledtelegrambot/WorkoutScheduledTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agr.workoutscheduledtelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutScheduledTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
