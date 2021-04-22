c7ea9590b2c77411e25cf95abd8ab3328125f76c
github_main <AGRabadanov@gmail.com>
2021-04-22 15:51:30 +0300

    downgrade TG bots lib

--- src/main/java/com/agr/workoutscheduledtelegrambot/WorkoutScheduledTelegramBotApplication.java
+++ src/main/java/com/agr/workoutscheduledtelegrambot/WorkoutScheduledTelegramBotApplication.java
@@ -3,11 +3,13 @@ package com.agr.workoutscheduledtelegrambot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 @EnableScheduling
 public class WorkoutScheduledTelegramBotApplication {
 	public static void main(String[] args) {
+		ApiContextInitializer.init();
 		SpringApplication.run(WorkoutScheduledTelegramBotApplication.class, args);
 	}
 }
