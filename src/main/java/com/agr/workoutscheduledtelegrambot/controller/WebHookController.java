c89c7ab09e150528bf488e23618da161c83e8fa8
github_main <AGRabadanov@gmail.com>
2021-04-22 16:31:56 +0300

    another test :/

--- src/main/java/com/agr/workoutscheduledtelegrambot/controller/WebHookController.java
+++ src/main/java/com/agr/workoutscheduledtelegrambot/controller/WebHookController.java
@@ -1,12 +1,13 @@
 package com.agr.workoutscheduledtelegrambot.controller;
 
 import com.agr.workoutscheduledtelegrambot.bot.WSTelegramBot;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-@Slf4j
 @RestController
 public class WebHookController {
     private final WSTelegramBot bot;
@@ -15,14 +16,19 @@ public class WebHookController {
         this.bot = bot;
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.POST)
-    public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
-        log.info("WebHookController.onUpdateReceived Update: " + update.toString());
-        return bot.onWebhookUpdateReceived(update);
-    }
+//    @RequestMapping(value = "/", method = RequestMethod.POST)
+//    public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
+//        log.info("WebHookController.onUpdateReceived Update: " + update.toString());
+//        return bot.onWebhookUpdateReceived(update);
+//    }
 
     @GetMapping("/test")
     public String test() {
         return "Test controller accessed";
+    }
+
+    @PostMapping("/")
+    public SendMessage onUpdateReceived(@RequestBody Update update) {
+        return bot.handleUpdate(update);
     }
 }
